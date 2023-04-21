7281f92c5c69f24ffa7ff7808481b119d22668ae
calvinlm <123354120+calvinlm@users.noreply.github.com>
2023-04-22 03:38:02 +0800

    Add files via upload

---
+++ finalprj/Blog.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="blog">
+        <div class="blog-container bd-grid">
+            <div class="blog-header">
+                <h2>{{ title }}</h2>
+            </div>
+        </div>
+        <div class="blog-title">
+            <h1>Creating a Single Page Application with Vue.js</h1>
+		    <p>Published on April 21, 2023</p>
+        </div>
+        <div class="blog-content">
+            <p>Single Page Applications (SPAs) are becoming increasingly popular as they provide a seamless user experience by loading content dynamically without refreshing the page. Vue.js is a great framework for building SPAs as it provides a powerful and flexible architecture for creating reactive and dynamic user interfaces.</p>
+			<p>One of the key benefits of using Vue.js for SPAs is its component-based architecture. Components are reusable and modular pieces of code that can be easily composed to build complex interfaces. Vue.js provides a simple and intuitive syntax for creating components, and its reactive data binding system makes it easy to update and manipulate data in real-time.</p>
+			<p>To create a simple SPA with Vue.js, we can start by defining a root Vue instance and mounting it to a DOM element. We can then define our components, which can be nested inside each other to create a hierarchical structure. We can use Vue.js directives to bind data to our components and update the UI as needed.</p>
+			<p>Vue.js also provides a powerful router that allows us to define and navigate between different views in our SPA. We can define routes and associate them with specific components, and then use the router-link component to create links that navigate between views.</p>
+			<p>With Vue.js, creating a SPA has never been easier. Its intuitive syntax, component-based architecture, and powerful router make it a great choice for building modern and dynamic user interfaces.</p>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Blog',
+    data () {
+        return{
+            title: 'Blog'
+        }
+    }
+  }
+  </script>
+
+<style>
+#blog {
+    width: 900px;
+    align-content: center;
+    display: inline-block;
+
+}
+blog-header{
+    margin-bottom: 30px;
+}
+
+</style>
---
+++ finalprj/Gallery.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="gallery">
+        <h1>{{ title }}</h1><br>
+        <h1>My first Single Page Application</h1><br>
+        <div class="gallery__img">
+            <div class="gridscroll">
+                <h3>Home page</h3>
+                <img v-bind:src="Home">
+                <h3>About page</h3>
+                <img v-bind:src="About">
+                <h3>Education Background page</h3>
+                <img v-bind:src="Edu">
+                <h3>Skills page</h3>
+                <img v-bind:src="Skills">
+                <h3>Contact page</h3>
+                <img v-bind:src="Contact">
+            </div>
+        </div>
+    </div>
+  </template>
+
+
+
+  <script>
+  import home_g from "/Documents/Calvin/WEBSERVER/Finals/finalprj/src/assets/Home.png"
+  import about_g from "/Documents/Calvin/WEBSERVER/Finals/finalprj/src/assets/About.png"
+  import edu_g from "/Documents/Calvin/WEBSERVER/Finals/finalprj/src/assets/Edu.png"
+  import skills_g from "/Documents/Calvin/WEBSERVER/Finals/finalprj/src/assets/Skills.png"
+  import contact_g from "/Documents/Calvin/WEBSERVER/Finals/finalprj/src/assets/contact.png"
+
+  export default {
+    name: 'Gallery',
+    data () {
+        return{
+            title: 'Gallery',
+            Home: home_g,
+            About: about_g,
+            Edu: edu_g,
+            Skills: skills_g,
+            Contact: contact_g
+        }
+    }
+  }
+  </script>
+
+<style>
+#gallery{
+    width: 900px;
+    display: inline-block;
+}
+img{
+    width: 100%;
+    display: inline-flex;
+    border-style: solid;
+    border-color: black;
+}
+h3{
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+</style>
